4bd85131ca13a6ff3f04a46f6a83850efbac97de
Ahmed Hassan Khamis <92717109+AhmedHassanKhamis@users.noreply.github.com>
2023-12-18 21:08:40 +0100

    Add files via upload

---
+++ php/php con jose/views/404.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'partials/cabecera.php';
+?>
+
+<div class="bg-body-tertiary p-5 rounded">
+    <h1>404</h1>
+    <p>
+        La pagina que estás buscando no existe.
+    </p>
+
+    <p><a href="/">Volver a la pagina de inicio</a></p>
+</div>
+<?php
+require 'partials/pie.php';
