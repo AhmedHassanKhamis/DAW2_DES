efcd42de857291a494f79b61a0fd08b7ff8e703f
Ahmed Hassan Khamis <92717109+AhmedHassanKhamis@users.noreply.github.com>
2024-02-14 20:52:16 +0100

    Add files via upload

--- NUEVO PROFE(FRANCISCO)/ejercicio_mysql/plantilla.php
+++ NUEVO PROFE(FRANCISCO)/ejercicio_mysql/plantilla.php
@@ -4,26 +4,45 @@ Para seleccionar el producto concreto utiliza un cuadro de selección dentro de 
  -->
 
 
-<?php 
+<?php
 require('variables.php');
 
-$conexion = new mysqli($server,$user,$password,$bbdd);
+$conexion = new mysqli($server, $user, $password, $bbdd);
 $error = $conexion->connect_errno;
-if ($error != null) {
-     echo "<p>Error $error conectando a la base de datos: $conexion->connect_error</p>";
-     exit();
-}else{
+$tablas = $conexion->query('select nombre_corto from producto')->fetch_all();
+$tablas2 = array_column($conexion->query('select nombre_corto from producto')->fetch_all(), 0);
+print_r($tablas);
 
-	$tablas = array_column($conexion->query('select nombre_corto from producto')->fetch_all(),0);
+print_r("<br><br><hr>");
+print_r($tablas2);
+
+if ($error != null) {
+	echo "<p>Error $error conectando a la base de datos: $conexion->connect_error</p>";
+	exit();
+	die();
+} else {
+
+	// $tablas = array_column($conexion->query('select nombre_corto from producto')->fetch_all(),0);
+
 	if (isset($_POST['submit'])) {
-		$tienda = array_column($conexion->query('select tienda.nombre,stock.unidades from stock,tienda,producto where tienda.cod = stock.tienda and producto.cod = stock.producto and producto.nombre_corto="'.$_POST['producto'] .'"')->fetch_all(),0);
-		$stock = array_column($conexion->query('select tienda.nombre,stock.unidades from stock,tienda,producto where tienda.cod = stock.tienda and producto.cod = stock.producto and producto.nombre_corto="'.$_POST['producto'] .'"')->fetch_all(),1);
+		$consulta =  $conexion->query('select tienda.nombre,stock.unidades,stock.tienda from stock,tienda,producto where tienda.cod = stock.tienda and producto.cod = stock.producto and producto.nombre_corto="' . $_POST['producto'] . '"')->fetch_all();
+		// $tienda = array_column($conexion->query('select tienda.nombre,stock.unidades from stock,tienda,producto where tienda.cod = stock.tienda and producto.cod = stock.producto and producto.nombre_corto="'.$_POST['producto'] .'"')->fetch_all(),0);
+		// $stock = array_column($conexion->query('select tienda.nombre,stock.unidades from stock,tienda,producto where tienda.cod = stock.tienda and producto.cod = stock.producto and producto.nombre_corto="'.$_POST['producto'] .'"')->fetch_all(),1);
+		$tienda = array_column($consulta, 0);
+		$stock = array_column($consulta, 1);
+		$tienda_id = array_column($consulta,2); 
 	}
+	if (isset($_POST['modificar'])) {
+		for ($i=0; $i < count($tienda) ; $i++) { 
+			$consulta =  $conexion->query('update stock where ')->fetch_all();
+		}
+	}
+
 	$conexion->close();
 }
 
-
-print_r("<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://www.w3.org/TR/html4/loose.dtd'>
+?>
+<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://www.w3.org/TR/html4/loose.dtd'>
 <html>
 
 <head>
@@ -34,34 +53,38 @@ print_r("<!DOCTYPE html PUBLIC '-//W3C//DTD HTML 4.01 Transitional//EN' 'http://
 
 <body>
 
-		<h1>PRODUCTO: </h1>
-		<form id='form_seleccion' action='' method='post'>
-			<select name='producto' id='producto'>
-			");
-			for ($i=1;$i <= count($tablas);$i++) {
-				print_r("<option value='".$tablas[$i]."'>".$tablas[$i]."</option>");
-			}
-			
-			print_r("
-			</select>
-			<input type='submit' name='submit'>
-		</form>
-		<hr>
-		<h2>STOCK EN TIENDA</h2>
-		<table>
-    <thead>
-      <tr>
-        <th>Tienda</th>
-        <th>Unidades</th>
-      </tr>
-    </thead>
-    <tbody align='center'>");
-	for ($i=0; $i < count($tienda); $i++) { 
-		print_r("<tr><td>".$tienda[$i]."</td><td>".$stock[$i]."</td></tr>");
-	}
-	print_r("
-    </tbody>
-  </table>
+	<h1>PRODUCTO: </h1>
+	<form id='form_seleccion' action='' method='post'>
+		<select name='producto' id='producto'>
+			<option value="<?= $_POST['producto'] ?>" default><?= $_POST['producto']?></option>
+			<?php
+				for ($i = 1; $i <= count($tablas); $i++) {
+					print_r("<option value='" . $tablas[$i] . "'>" . $tablas[$i] . "</option>");
+				}
+			?>
+		</select>
+		<input type='submit' name='submit'>
+	</form>
+	<hr>
+	<h2>STOCK EN TIENDA</h2>
+	<table>
+		<thead>
+			<tr>
+				<th>Tienda</th>
+				<th>Unidades</th>
+			</tr>
+		</thead>
+		<tbody align='center'>
+			<form action="#" method="post">
+			<?php
+				for ($i = 0; $i < count($tienda); $i++) {
+					print_r("<tr><td>" . $tienda[$i] . "</td><td><input type='number' name='".$tienda[$i]."' min='0' value='".$stock[$i]."'></td></tr>");
+				}
+			?>
+			<tr><td colspan="2"><input type="submit" name="modificar" value="modificar"></td></tr>
+			</form>
+		</tbody>
+	</table>
 </body>
-</html>");
-?>
+
+</html>
